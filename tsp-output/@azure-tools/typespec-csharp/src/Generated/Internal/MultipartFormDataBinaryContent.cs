2905f1f4ad476452b140e44db9b80f208a3eca58
Anne Thompson <annelo@microsoft.com>
2024-03-26 14:31:33 -0700

    updates

--- tsp-output/@azure-tools/typespec-csharp/src/Generated/Internal/MultipartFormDataBinaryContent.cs
+++ tsp-output/@azure-tools/typespec-csharp/src/Generated/Internal/MultipartFormDataBinaryContent.cs
@@ -12,7 +12,7 @@ using System.Net.Http.Headers;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace OpenAI.Emitted;
+namespace OpenAI;
 
 internal class MultipartFormDataBinaryContent : BinaryContent
 {
