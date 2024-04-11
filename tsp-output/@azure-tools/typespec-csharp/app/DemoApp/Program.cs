9c4abdf4b4d56379153677617015dc39d8c78af9
Anne Thompson <annelo@microsoft.com>
2024-04-11 09:04:12 -0700

    reorganize demo app code

--- tsp-output/@azure-tools/typespec-csharp/app/DemoApp/Program.cs
+++ tsp-output/@azure-tools/typespec-csharp/app/DemoApp/Program.cs
@@ -3,44 +3,77 @@ using OpenAI.Models;
 using System.ClientModel;
 
 using AzureOpenAI.Models;
+using AzureOpenAI;
 
 Console.WriteLine("Hello, World!");
 
-//Uri endpoint = new("https://annelo-openai-01.openai.azure.com/");
-//string apiKey = Environment.GetEnvironmentVariable("AZURE_OPENAI_KEY")!;
+CallUnbrandedService();
+CallAzureService();
 
-string apiKey = Environment.GetEnvironmentVariable("OPENAI_KEY")!;
-
-OpenAIClient client = new OpenAIClient(new ApiKeyCredential(apiKey));
-Chat chatClient = client.GetChatClient();
-
-List<BinaryData> messages  = new List<BinaryData>();
-var message = new
+void CallUnbrandedService()
 {
-    role = "user",
-    content = "running over the same old ground"
-};
-messages.Add(BinaryData.FromObjectAsJson(message));
+    // <Third party>
+    string apiKey = Environment.GetEnvironmentVariable("OPENAI_KEY")!;
 
-// Add Azure property
-AzureSearchChatExtensionParameters searchParams = new(new Uri("https://azure.search.com"), "MySearchIndex");
-AzureSearchChatExtensionConfiguration dataSource = new(searchParams);
+    OpenAIClient client = new(new ApiKeyCredential(apiKey));
+    // </Third party>
 
-CreateChatCompletionRequest request = new CreateChatCompletionRequest(messages, CreateChatCompletionRequestModel.Gpt35Turbo);
-request.SetAzureDataSource(dataSource);
+    Chat chatClient = client.GetChatClient();
 
-ClientResult<CreateChatCompletionResponse> result = chatClient.CreateChatCompletion(request);
-CreateChatCompletionResponse completion = result.Value;
+    List<BinaryData> messages = new();
+    var message = new
+    {
+        role = "user",
+        content = "running over the same old ground"
+    };
+    messages.Add(BinaryData.FromObjectAsJson(message));
 
-if (completion.Choices[0].Message.GetAzureExtensionsContext().Citations.Count > 0)
-{
-    // TODO: Do a thing
+    CreateChatCompletionRequest request = new(messages, CreateChatCompletionRequestModel.Gpt35Turbo);
+
+    ClientResult<CreateChatCompletionResponse> result = chatClient.CreateChatCompletion(request);
+    CreateChatCompletionResponse completion = result.Value;
+
+    // TODO: Do something with output
 }
 
+void CallAzureService()
+{
+    // <Azure>
+    Uri endpoint = new("https://annelo-openai-01.openai.azure.com/");
+    string apiKey = Environment.GetEnvironmentVariable("AZURE_OPENAI_KEY")!;
 
-//AzureOpenAIClient client = new AzureOpenAIClient(endpoint, new ApiKeyCredential(apiKey));
-//Chat chatClient = client.GetChatClient("gpt-35-turbo-instruct");
+    AzureOpenAIClient client = new(endpoint, new ApiKeyCredential(apiKey));
+    // </Azure>
 
-//ClientResult<ChatCompletion> result = await chatClient.CompleteChatAsync()
+    Chat chatClient = client.GetChatClient();
 
-// TODO: MRW story - input model and output models too.
+    List<BinaryData> messages = new();
+    var message = new
+    {
+        role = "user",
+        content = "running over the same old ground"
+    };
+    messages.Add(BinaryData.FromObjectAsJson(message));
+
+    CreateChatCompletionRequest request = new(messages, CreateChatCompletionRequestModel.Gpt35Turbo);
+
+    // <Azure>
+    // Add Azure input property via extension methods
+    AzureSearchChatExtensionParameters searchParams = new(new Uri("https://azure.search.com"), "MySearchIndex");
+    AzureSearchChatExtensionConfiguration dataSource = new(searchParams);
+    request.SetAzureDataSource(dataSource);
+    // </Azure>
+
+    ClientResult<CreateChatCompletionResponse> result = chatClient.CreateChatCompletion(request);
+    CreateChatCompletionResponse completion = result.Value;
+
+    // <Azure>
+    // Use Azure output property via extension methods
+    AzureChatExtensionsMessageContext? azureContext = completion.Choices[0].Message.GetAzureExtensionsContext();
+
+    if (azureContext is not null && azureContext.Citations.Count > 0)
+    {
+        // TODO: Do something with output
+    }
+    // </Azure>
+}
